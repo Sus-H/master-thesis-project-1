92d9f7c808467b6dc39db18b0f7fb5c657d3e8fd
Hampus Siik <hampus.siik@gmail.com>
2025-03-17 20:07:28 +0100

    refactor: clean up JSX formatting in Home and TopBar components

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -12,31 +12,32 @@ export default function Home() {
         <div className="">
           <Scroll_area></Scroll_area>
         </div>
-        <div className="components-mind-map-view">
-          <div className="h-screen w-screen">
-            <ReactFlowProvider>
-              <MindMap></MindMap>
-            </ReactFlowProvider>
+        <div className="h-screen w-screen">
+          <ReactFlowProvider>
+            <MindMap></MindMap>
+          </ReactFlowProvider>
+        </div>
+        <div className="bg-white h-screen px-10 py-5">
+          <p className="text-2xl">Visa summering</p>
+          <div className="">
+
+            <p>Summering här borta</p>
+            <p>Involverade människor: 3 patienter</p>
+            <p>Involverade Fordon: 1 Bil (Volvo XC60), krock</p>
+            <p>Resursplan: 3 Ambulanser</p>
+            <p>
+              1 Ambulans: Patient (Främre passagerare) blödning,
+              fraktur, stabil andning, Chans för överlevnad 40%
+            </p>
+            <p>
+              1 Ambulans: Patient (förare), Chans för överlevnad 3%{" "}
+            </p>
+            <p>
+              1 Ambulans: Patient, (Bakre passagerare)Chans för
+              överlevnad 3%{" "}
+            </p>
           </div>
         </div>
-        {/* <div className="summary w-[400px] absolute right-0 z-10 bg-white h-screen px-10 py-5">
-          <p className="text-2xl">Visa summering</p>
-          <p>Summering här borta</p>
-          <p>Involverade människor: 3 patienter</p>
-          <p>Involverade Fordon: 1 Bil (Volvo XC60), krock</p>
-          <p>Resursplan: 3 Ambulanser</p>
-          <p>
-            1 Ambulans: Patient (Främre passagerare) blödning,
-            fraktur, stabil andning, Chans för överlevnad 40%
-          </p>
-          <p>
-            1 Ambulans: Patient (förare), Chans för överlevnad 3%{" "}
-          </p>
-          <p>
-            1 Ambulans: Patient, (Bakre passagerare)Chans för
-            överlevnad 3%{" "}
-          </p>
-        </div> */}
       </div>
     </div>
   );
--- components/topBar.tsx
+++ components/topBar.tsx
@@ -7,18 +7,18 @@ export function TopBar() {
     <div className="z-6 p-2">
       <div className="flex items-center mx-5 justify-between">
         <div>
-          <p><img src="images/DHS_Icon.svg" alt="" className="inline"/> Min nya simulering</p>
+          <p><img src="images/DHS_Icon.svg" alt="" className="inline" /> Min nya simulering</p>
         </div>
         <div className="flex gap-5">
-          <button className="hover:underline"><DropdownMenuComponents/></button>
-          
+          <button className="hover:underline"><DropdownMenuComponents /></button>
+
           <NavButton to="/page_algorithm_browser"> <img src="images/Book_open.svg" className="inline" /> Algoritm Utforskaren</NavButton>
           <button className="hover:underline"> <img src="images/Folder_plus.svg" className="inline" /> Importera Extern Data </button>
         </div>
         <div className="flex gap-5">
-          <button className="hover:underline text-white bg-sky-500 px-2 py-1 rounded"><img src="images/Share.svg" className="inline"/> Ladda ner</button>
-          <button className="hover:underline text-white bg-sky-500 px-2 py-1 rounded"><img src="images/Share_2.svg" className="inline"/> Dela</button>
-          <button className="hover:underline text-white bg-sky-500 px-2 py-1 rounded"><img src="images/Download.svg" className="inline"/> Exportera</button>
+          <button className="hover:underline text-white bg-sky-500 px-2 py-1 rounded"><img src="images/Share.svg" className="inline" /> Ladda ner</button>
+          <button className="hover:underline text-white bg-sky-500 px-2 py-1 rounded"><img src="images/Share_2.svg" className="inline" /> Dela</button>
+          <button className="hover:underline text-white bg-sky-500 px-2 py-1 rounded"><img src="images/Download.svg" className="inline" /> Exportera</button>
         </div>
       </div>
     </div>
