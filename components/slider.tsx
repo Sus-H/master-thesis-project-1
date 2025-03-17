8e18488bda01e3a4dcb420f0509d207a93330e47
MeepShake <susselelle@hotmail.com>
2025-03-17 20:58:35 +0100

    styled parameter slider

--- components/slider.tsx
+++ components/slider.tsx
@@ -1,33 +1,33 @@
 import { Slider } from "radix-ui";
 
 export function SliderComponent({ name }: { name: string }) {
-  const steps = 10;
+  const steps = 5;
   const max = 100;
   const stepSize = max / steps;
   return (
     <div>
       <p>{name}</p>
       <Slider.Root
-        className="relative flex h-5 w-[200px] touch-none select-none items-center"
+        className="relative flex h-5 w-[200px] touch-none select-none content-center place-items-center"
         defaultValue={[50]}
         max={max}
         step={stepSize}
       >
-        <Slider.Track className="relative h-[10px] grow rounded-full bg-[#EEE]">
-          <Slider.Range className="absolute h-2 rounded-full bg-[#6E56CF]" />
-          {Array.from({ length: steps + 1 }).map((_, index) => (
+        <Slider.Track className="relative h-3 grow rounded-full bg-[#EEE]">
+          <Slider.Range className="absolute h-3 rounded-full bg-[#6E56CF]" />
+          {Array.from({ length: steps - 1 }).map((_, index) => (
             <div
               key={index}
-              className="absolute h-1 w-1 rounded-full bg-white"
+              className="absolute h-1 w-1 rounded-full bg-purple-300 inset-y-1"
               style={{
                 left: `${(index / steps) * 100}%`,
-                transform: "translateX(-50%)",
+                transform: "translateX(1000%)",
               }}
             />
           ))}
         </Slider.Track>
         <Slider.Thumb
-          className="block border-3 border-purple-300  size-5 rounded-full bg-[#6E56CF] shadow-[0_2px_10px] shadow-blackA4 hover:bg-violet3 focus: focus: focus:outline-none"
+          className="block border-3 border-purple-300 inset-y-0 size-5 rounded-full bg-[#6E56CF] shadow-[0_2px_10px] shadow-purple-950   hover:bg-violet3 focus: focus: focus:outline-none"
           aria-label="Volume"
         />
       </Slider.Root>
